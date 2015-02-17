9c22c45adbbe96616b7c10cba5544b82d7f6497e
Anders Engström <anders.engstrom@hrmsoftware.se>
2015-02-17 17:59:12 +0100

    Add two 'functional' tests.

--- workshop/workshop-clj/test/workshop_clj/functions_test.clj
+++ workshop/workshop-clj/test/workshop_clj/functions_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [workshop-clj.functions :refer :all]))
 
-; (deftest a-test
-;   (testing "FIXME, I fail."
-;     (is (= 0 1))))
+
+(deftest negate-test
+  (defn larger-than-5? [n] (> n 5))
+
+  (testing "a predicate"
+    (let [less-or-eql-to5? (negate larger-than-5?)]
+      (is (less-or-eql-to5? 2))
+      (is (less-or-eql-to5? 5))
+      (is (not (less-or-eql-to5? 10))))))
+
+(deftest flipped-test
+  (defn subtract [a b] (- a b))
+
+  (testing "with subtraction"
+    (let [flipped (flip subtract)]
+      (is (= 6 (subtract 10 4)))
+      (is (= -6 (flipped 10 4))))))
