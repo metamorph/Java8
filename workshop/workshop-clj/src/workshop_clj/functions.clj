bdd344b23d2dca6f3097449b5dc1c85deef5b2f1
Anders Engström <anders.engstrom@hrmsoftware.se>
2015-02-17 17:59:12 +0100

    Add tests for 'functional'

--- workshop/workshop-clj/src/workshop_clj/functions.clj
+++ workshop/workshop-clj/src/workshop_clj/functions.clj
@@ -1,3 +1,18 @@
 (ns workshop-clj.functions)
 
+(defn negate 
+  "Return a function that will swap the return value of the provided function (true -> false)" 
+  [f] 
+  nil)
+
+(defn flip 
+  "Returns a new function taking two argument in the flipped order
+   of 'f'." 
+  [f] 
+  nil)
+
+(defn compose
+  "Return a function that is a composition of the two provided funcs"
+  [f1 f2]
+  nil)
 
--- workshop/workshop-clj/test/workshop_clj/functions_test.clj
+++ workshop/workshop-clj/test/workshop_clj/functions_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [workshop-clj.functions :refer :all]))
 
-; (deftest a-test
-;   (testing "FIXME, I fail."
-;     (is (= 0 1))))
+(defn larger-than-5? "Check if a number is larger than 5" [n] (> n 5))
+(defn str-length "Get the number of chars in a string" [s] (count s))
+(defn subtract [a b] (- a b))
+
+(deftest negate-test
+  (testing "a predicate"
+    (let [less-or-eql-to5? (negate larger-than-5?)]
+      (is (less-or-eql-to5? 2))
+      (is (less-or-eql-to5? 5))
+      (is (not (less-or-eql-to5? 10))))))
+
+(deftest flipped-test
+  (testing "with subtraction"
+    (let [flipped (flip subtract)]
+      (is (= 6 (subtract 10 4)))
+      (is (= -6 (flipped 10 4))))))
+
+(deftest compose-test
+  (let [longer-than-5? (compose str-length larger-than-5?)]
+    (is (longer-than-5? "123456"))
+    (is (not (longer-than-5? "abc")))))
