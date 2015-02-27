728f03746e52941e301380ccc128770a5148bdc0
Henrik Grankvist <henrik.grankvist@gmail.com>
2015-02-27 15:27:45 +0100

    Added more exercises about lambdas and streams.
    Imperatively implement some of the functions from last workshop.
    
    And more on treating lambdas and applying them on streams.

--- workshop/workshop-ex/src/main/java/se/hrmsoftware/model/PersonRepository.java
+++ workshop/workshop-ex/src/main/java/se/hrmsoftware/model/PersonRepository.java
@@ -17,11 +17,12 @@ public class PersonRepository {
     }
 
     private static final List<Person> defaultPeople = Arrays.asList(
-            new Person("Jussi Karjalajnen", 89, "jussi@eisapeita.fi"),
             new Person("Urban Hult", 23, "urban@hult.se"),
-            new Person("Jörgen Hult", 29, "jorgen@hult.se"),
-            new Person("Anna Hult", 21, "anna@hult.se"),
-            new Person("Åsa Råmson", 35, "slangpengarnaisjon@regeringen.se"));
+			new Person("Jussi Karjalajnen", 89, "jussi@eisapeita.fi"),
+			new Person("Jörgen Hult", 29, "jorgen@hult.se"),
+            new Person("Anna Jult", 21, "anna@jult.se"),
+            new Person("Åsa Råmson", 35, "slangpengarnaisjon@regeringen.se"),
+			new Person("Jussi Karjalajnen jr", 21, "jussijr@eisapeita.fi"));
 
 
     //Mutatable list of people in this repository.
