5bc5b60d332d68924e47a77f3d0b821dd1b3feed
Anders Engström <anders.engstrom@hrmsoftware.se>
2015-05-29 16:13:40 +0200

    Uno testo

--- workshop/workshop-ex/src/main/java/se/hrmsoftware/exercises1/no6/Temperatures.java
+++ workshop/workshop-ex/src/main/java/se/hrmsoftware/exercises1/no6/Temperatures.java
@@ -2,6 +2,8 @@ package se.hrmsoftware.exercises1.no6;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -19,6 +21,25 @@ public class Temperatures {
 	 */
 	public static Set<ChartEntry> calculateAverageTemperaturByPopulation(Collection<CityData> data) {
 		return Collections.emptySet();
+	}
+
+	/**
+	 * Utility method that might useful ..
+	 */
+	private static ChartEntry toEntry(CityData data) {
+		return null;
+	}
+
+	/**
+	 * From the list of CityData points, find the city with the population that is 'closest' to the
+	 * specified population. If more than one city are equally 'close' to the 'forPopulation' return the
+	 * 'smallest' city.
+	 * @param data the data points.
+	 * @param forPopulation the population for which to find the matching CityEntry
+	 * @return a ChartEntry or Optional.empty if no entry can be found.
+	 */
+	public static Optional<ChartEntry> findAndMapClosestPoint(Collection<CityData> data, int forPopulation) {
+		return Optional.empty();
 	}
 
 }
--- workshop/workshop-ex/src/test/java/se/hrmsoftware/exercises1/no6/TemperaturesTest.java
+++ workshop/workshop-ex/src/test/java/se/hrmsoftware/exercises1/no6/TemperaturesTest.java
@@ -5,10 +5,20 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Supplier;
+
+import static se.hrmsoftware.exercises1.no6.Temperatures.findAndMapClosestPoint;
 
 public class TemperaturesTest extends TestCase {
+
+	private final List<CityData> input = Arrays.asList(
+			new CityData("Alvesta", 5000, -2, 3, 5, 10, 15, 20, 19, 16, 4, 0),
+			new CityData("Vislanda", 2000, -5, 5, 7, 12, 17, 22, 21, 18, 6, 2),
+			new CityData("Lammhult", 1300, -0, 1, 3, 7, 12, 18, 13, 8, -2, -6),
+			new CityData("Stockholm", 9000000, -4, -3, 2, 3, 15, 21, 22, 23, 2, 1, 3, 0));
 
 	private static double averageForPopulation(Collection<ChartEntry> data, int population) {
 		return data.stream()
@@ -21,11 +31,6 @@ public class TemperaturesTest extends TestCase {
 	@Test
 	public void testCalculateAverageTemperatureByPopulation() throws Exception {
 
-		List<CityData> input = Arrays.asList(
-				new CityData("Alvesta", 5000, -2, 3, 5, 10, 15, 20, 19, 16, 4, 0),
-				new CityData("Vislanda", 2000, -5, 5, 7, 12, 17, 22, 21, 18, 6, 2),
-				new CityData("Lammhult", 1300, -0, 1, 3, 7, 12, 18, 13, 8, -2, -6),
-				new CityData("Stockholm", 9000000, -4, -3, 2, 3, 15, 21, 22, 23, 2, 1, 3, 0));
 		Set<ChartEntry> output = Temperatures.calculateAverageTemperaturByPopulation(input);
 
 		// Check data.
@@ -36,5 +41,13 @@ public class TemperaturesTest extends TestCase {
 		assertEquals(7.08d, averageForPopulation(output, 9000000), 0.1);
 	}
 
+	@Test
+	public void testFindAndMap() {
+		Supplier<RuntimeException> boom = () -> new RuntimeException("boom");
 
+		assertEquals(5.4d, findAndMapClosestPoint(input, 1000).orElseThrow(boom).getAverageTemperature(), 0.01);
+		assertEquals(10.5d, findAndMapClosestPoint(input, 3000).orElseThrow(boom).getAverageTemperature(), 0.01);
+		assertFalse(findAndMapClosestPoint(Collections.emptyList(), 1300).isPresent());
+		assertFalse(findAndMapClosestPoint(input, 3500).isPresent());
+	}
 }
